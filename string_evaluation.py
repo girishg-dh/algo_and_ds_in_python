318ffe499f1694c4e4bb9bd243110518f11948cd
girish-dh <114143528+girishg-dh@users.noreply.github.com>
2023-12-03 02:37:08 +0100

    stacks

---
+++ string_evaluation.py
@@ -0,0 +1,14 @@
+"""
+Statement
+Given a string containing an arithmetic expression, implement a basic calculator that evaluates the expression string. The expression string can contain integer numeric values and should be able to handle the “+” and “-” operators, as well as “()” parentheses.
+
+Constraints:
+
+Let s be the expression string. We can assume the following constraints: 
+s consists of digits, “+”, “-”, “(”, and “)”.
+s represents a valid expression.
+“+” is not used as a unary operation (+1 and +(2+3) +(2+3) are invalid).
+“-” could be used as a unary operation (−1−1 and −(2+3) −(2+3)are valid).
+There will be no two consecutive operators in the input.
+Every number and running calculation will fit in a signed 32-bit integer.
+"""
