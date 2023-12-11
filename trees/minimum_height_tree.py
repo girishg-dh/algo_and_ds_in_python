273abb9559f91ce4e2d14ac592af0c2f0b211ad6
Girish Gupta <114143528+girishg-dh@users.noreply.github.com>
2023-12-12 00:05:49 +0100

    update

--- trees/minimum_height_tree.py
+++ trees/minimum_height_tree.py
@@ -1,32 +1,34 @@
-def adjacency_list(n, edges):
-    adj = [set() for i in range(n)]
+def create_adjacency_list(n:int, edges:list):
+    adj = [set() for _ in range(n)]
 
     for v1, v2 in edges:
         adj[v1].add(v2)
         adj[v2].add(v1)
-
     return adj
+
 def min_height_trees(n, edges):
     if n <= 2:
-        return [i for i in range(n)]
+        return [i for i in edges]
 
-    adj = adjacency_list(n, edges)
+    adj = create_adjacency_list(n, edges)
+
     leaves = [i for i in range(n) if len(adj[i]) == 1]
 
-    rem_nodes = n
-    while rem_nodes > 2:
-        rem_nodes -= len(leaves)
-        temp_leaves = []
+    remaining_nodes = n
+
+    while remaining_nodes > 2:
+        remaining_nodes -= len(leaves)
+        next_leaves = []
 
         while leaves:
-            leaf = leaves.pop()
-            neighbour = adj[leaf].pop()
-            adj[neighbour].remove(leaf)
+            current_leaf = leaves.pop()
+            neighbour = adj[current_leaf].pop()
+            adj[neighbour].remove(current_leaf)
 
+            #if after removal of current leaf remaining connection is 1 then neighbour is on edge
             if len(adj[neighbour]) == 1:
-                temp_leaves.append(neighbour)
-
-        leaves = temp_leaves
+                next_leaves.append(neighbour)
+        leaves = next_leaves
     return leaves
 
 
