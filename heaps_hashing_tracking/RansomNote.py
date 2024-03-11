64c39ea25eb7b6df091ba5e8101327d7ff676574
Girish Gupta <114143528+girishg-dh@users.noreply.github.com>
2024-03-11 01:51:01 +0100

    subset

--- heaps_hashing_tracking/RansomNote.py
+++ heaps_hashing_tracking/RansomNote.py
@@ -5,9 +5,19 @@ Return TRUE if it can be constructed, FALSE otherwise.
 '''
 from collections import Counter
 
-def can_construct(ransom_note, magazine):
+def can_construct_simple(ransom_note, magazine):
     r, m = Counter(ransom_note), Counter(magazine)
     return r <= m
+
+def can_construct(ransom_note, magazine):
+    r, m = Counter(ransom_note), Counter(magazine)
+    for k, v in r.items():
+        if m[k] - v >= 0 :
+            m[k] -= v
+        else:
+            return False
+    return True
+
 
 print(can_construct("baad", "abcd"))
 print(can_construct("bad", "abcd"))
