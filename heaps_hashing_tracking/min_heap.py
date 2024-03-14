5bf240006378fe94ed1bedd18999dc64c797724f
Girish Gupta <114143528+girishg-dh@users.noreply.github.com>
2024-03-14 21:03:17 +0100

    Two heap problem

---
+++ heaps_hashing_tracking/min_heap.py
@@ -0,0 +1,17 @@
+from heapq import *
+class MinHeap:
+    def __init__(self) -> None:
+        self.heap_list = []
+
+    def insert(self, x):
+        heappush(self.heap_list, x)
+
+    def get_top(self):
+        return self.heap_list[0]
+
+    def __str__(self) -> str:
+        out = '['
+        for i in self.heap_list:
+            out = out + str(i)+', '
+        out = out[:-2]+']'
+        return out
