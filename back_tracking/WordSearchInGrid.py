95b2eec2cb22714eeb803bdf05b58d3afd05c858
girish-dh <114143528+girishg-dh@users.noreply.github.com>
2024-04-13 23:53:49 +0200

    Word Search in 2D Grid

--- back_tracking/WordSearchInGrid.py
+++ back_tracking/WordSearchInGrid.py
@@ -1,8 +1,35 @@
+def grid_to_uppper(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            grid[i][j] = grid[i][j].upper()
+    return grid
 
-from neighbours import neighbours
-from backtracking import backtracking
+def dfs(grid, word, row, col, index, visited):
+    if index == len(word): 
+        return True 
+    if(0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == word[index] and not visited[row][col]):
+        visited[row][col] = True 
+        if (dfs(grid, word, row, col + 1, index + 1, visited) or 
+            dfs(grid, word, row + 1, col, index + 1, visited) or 
+            dfs(grid, word, row - 1, col, index + 1, visited) or 
+            dfs(grid, word, row, col - 1, index + 1, visited)):
+            return True
+        visited[row][col] = False
+    return False
+
 
 def word_search(grid, word):
+   if not grid or not word:
+       return False
+   
+   word = word.upper()
+   grid = grid_to_uppper(grid)
+   visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
+   for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if dfs(grid, word, i, j, 0, visited):
+                return True
+   return False
     
 
 
--- back_tracking/neighbours.py
+++ back_tracking/neighbours.py
@@ -1,15 +1,30 @@
-class neighbours:
+class Neighbours:
     def __init__(self) -> None:
         self.grid = [[]]
 
     def left(grid, x, y):
-        return grid[x][y-1]
+        if y > 0:
+            return grid[x][y-1]
+        else:
+            return None
     
     def right(grid, x, y):
-        return grid[x][y+1]
+        if y < len(grid[x])-1:
+            return grid[x][y+1]
+        else:
+            return None 
     
-    def up(grid, x, y):
-        return grid[x-1][y]
+    def up(self, grid, x, y):
+        if x > 0:
+            return grid[x-1][y]
+        else:
+            return None
     
-    def down(grid, x, y):
-        return grid[x+1][y]
+    def down(self, grid, x, y):
+        if x < len(grid)-1:
+            return grid[x+1][y]
+        else:
+            return None
+
+    def all(self, grid, x, y):
+        return [self.left(grid, x, y), self.right(grid, x, y), self.up(grid, x, y), self.down(grid, x, y)]
