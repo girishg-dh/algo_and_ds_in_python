7b8daaeaa4ee61a0b7b21bf5a670f9401a603bcf
girish-dh <114143528+girishg-dh@users.noreply.github.com>
2024-04-17 00:50:07 +0200

    Combine Sum

--- dynamic_programing/CombinationSum.py
+++ dynamic_programing/CombinationSum.py
@@ -4,7 +4,19 @@ def combination_sum(nums, target)->list:
     :type target: int
     :rtype: int
     """
-    
+    table = [[] for _ in range(target + 1)]
+    table[0] = [[]]
+
+    for i in range(1, target + 1):
+         for n in nums:
+              if n <= i:
+                   for c in table[i - n]:
+                        temp = sorted(c + [n]) 
+                        if temp not in table[i]:
+                             table[i].append(temp)
+                        
+    return table[target]
+
 
 # Driver code
 def main():
